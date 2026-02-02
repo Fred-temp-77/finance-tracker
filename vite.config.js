1a785e786280e1b9c4e4e787e4ea435fa72a8b20
Fred-temp-77 <fklatigogo.777@gmail.com>
2026-02-02 23:10:53 +0100

    Third Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Fred-temp-77/finance-tracker.git',
   plugins: [tailwindcss(), react()],
 })
